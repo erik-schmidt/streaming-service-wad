462a88b82cf6d7f7b32d54d68285153c1415f243
Erik Schmidt <47977947+erschmidd@users.noreply.github.com>
2021-06-24 12:07:10 +0200

    initial commit

---
+++ model/movie.js
@@ -0,0 +1,8 @@
+function Movie(name, director, year, actors, description) {
+    var self = this;
+    self.name = name;
+    self.director = director;
+    self.year = year;
+    self.actors = actors;
+    self.description = description;
+}
